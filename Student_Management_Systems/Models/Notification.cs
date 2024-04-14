50600b8f54b4910c631f60200ea3e5fc3198fe32
Yamini Alugubelli <yaminireddyallugubeli.70@gmail.com>
2024-04-14 18:59:08 -0400

    Minor modification in Notification class

--- Student_Management_Systems/Models/Notification.cs
+++ Student_Management_Systems/Models/Notification.cs
@@ -9,16 +9,16 @@ namespace Student_Management_Systems.Models
     public class Notification
     {
         public int NotificationId { get; set; }
-        public int SenderUserId { get; set; } 
+        public int UserId { get; set; } 
         public string Message { get; set; }
         public DateTime Date { get; set; }
 
         public Users Sender { get; set; }
 
-        public Notification(int notificationId, int senderUserId, string message, DateTime date, Users sender)
+        public Notification(int notificationId, int userId, string message, DateTime date, Users sender)
         {
             NotificationId = notificationId;
-            SenderUserId = senderUserId;
+            UserId = userId;
             Message = message;
             Date = date;
             Sender = sender;
